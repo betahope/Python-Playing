c90f8882ce754e5ba8a1cdc717ca1ca808cd240b
Developer Hope <me@devhope.co>
2020-07-14 07:37:13 +0000

    First CI exercises with python3 and terminal

---
+++ first.py
@@ -0,0 +1,12 @@
+# First exercise
+def print_message(message):
+    print(message)
+
+
+print_message("Hello world")
+
+# Second exercise
+my_list = [1, 2, 3, 4, 5]
+
+for item in my_list:
+    print(item)
